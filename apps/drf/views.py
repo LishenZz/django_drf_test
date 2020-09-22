d06fbfabd7c195eb29f63367c7ab36fa4aed470d
e <1048900106@qq.com>
2020-09-22 18:05:53 +0800

    1

--- apps/drf/views.py
+++ apps/drf/views.py
@@ -4,11 +4,11 @@ from django.contrib.auth.models import User
 from rest_framework import routers,serializers,viewsets
 
 # Create your views here.
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ['url','username','email','is_staff']
-
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+# class UserSerializer(serializers.HyperlinkedModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = ['url','username','email','is_staff']
+#
+# class UserViewSet(viewsets.ModelViewSet):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
