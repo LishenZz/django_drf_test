39a8faa6210d1db65933bb0028d87d2b8e79edee
LishenZz <630408856@qq.com>
2020-09-29 00:00:42 +0800

    1

--- django_drf_test/settings.py
+++ django_drf_test/settings.py
@@ -40,12 +40,16 @@ AUTH_USER_MODEL = 'users.UserProfile'
 
 INSTALLED_APPS = [
     'apps.users',
+    'apps.goods',
+    'apps.trade',
+    'apps.user_operation',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'DjangoUeditor',
     'rest_framework',
 ]
 
@@ -87,8 +91,8 @@ WSGI_APPLICATION = 'django_drf_test.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME':'book',
-        'USER':'book',
+        'NAME':'xadmin',
+        'USER':'xadmin',
         'HOST':'121.199.69.95',
         'PORT':'3306',
         'PASSWORD':'jkl5201314',
@@ -119,15 +123,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
